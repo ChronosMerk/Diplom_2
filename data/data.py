7a061050e73b6b8e77717dce06b24d20a5fa0823
Chronos <merk235@gmail.com>
2025-09-03 01:28:45 +0500

    Покрытие тестами все что связано с user данными

---
+++ data/data.py
@@ -0,0 +1,13 @@
+class URLS:
+    BASE = 'https://stellarburgers.nomoreparties.site/api'
+
+class AuthorizationUser:
+     JSON_USER = {
+        "email": "ahuesf@gmail.com",
+        "password": "AHUESF"
+    }
+
+class CreateOrderData:
+    JSON_ORDER = {
+
+    }
---
+++ data/data_message.py
@@ -0,0 +1,7 @@
+class RegistrationUserMessage:
+    MESSAGE_REGISTER_EXISTING_USER = 'User already exists'
+    MESSAGE_REGISTRATION_WITHOUT_FILLING_IN_THE_FIELD = 'Email, password and name are required fields'
+
+class LoginUserMessage:
+    MESSAGE_USER_PASSWORD_OR_EMAIL_INCORRECT = 'email or password are incorrect'
+    MESSAGE_USER_NOT_AUTH = 'You should be authorised'

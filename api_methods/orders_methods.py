609cc244e9c2fc4dbcb428bcd7449f2d8557678a
Chronos <merk235@gmail.com>
2025-09-04 01:42:18 +0500

    Покрытие тестами все что связано с orders

--- api_methods/orders_methods.py
+++ api_methods/orders_methods.py
@@ -6,7 +6,13 @@ class OrdersMethods:
         self.url = url
 
     @allure.step('Создание заказа')
-    def create_orders(self, payload, token=None):
+    def create_orders(self, payload=None, token=None):
         headers = {"Authorization": f"{token}"}
         response = requests.post(self.url, json=payload, headers=headers)
+        return response
+
+    @allure.step('Получение заказа пользователя')
+    def receiving_order(self, token=None):
+        headers = {"Authorization": f"{token}"}
+        response = requests.get(self.url, headers=headers)
         return response
--- data/data_message.py
+++ data/data_message.py
@@ -5,3 +5,7 @@ class RegistrationUserMessage:
 class LoginUserMessage:
     MESSAGE_USER_PASSWORD_OR_EMAIL_INCORRECT = 'email or password are incorrect'
     MESSAGE_USER_NOT_AUTH = 'You should be authorised'
+
+class CreateOrdersMessage:
+    MESSAGE_CREATE_ORDERS_INCORRECT_HASH_INGREDIENT = 'One or more ids provided are incorrect'
+    MESSAGE_CREATE_ORDERS_NOT_INGREDIENT = 'Ingredient ids must be provided'
